f677a88b1d23bbe025df9a7a5b55007ac91cf61e
ferlobo1985 <fernando.lobo.prez@gmail.com>
2022-05-03 03:20:55 -0300

    06-Formik-Components

--- src/components/formThree.js
+++ src/components/formThree.js
@@ -1,22 +1,52 @@
 import React from 'react';
+import { Formik, Form, Field, ErrorMessage } from 'formik'
+import * as Yup from 'yup';
 
 const FormThree = () => {
+
+    const formikProps = {
+        initialValues:{firstname:'',color:''},
+        validationSchema:Yup.object({
+            firstname:Yup.string().required('Sorry, this is required')
+        }),
+        onSubmit: values => {
+            console.log(values)
+        }
+    }
+
+
     return(
         <div className="container">
             <div className="col-md-12 mt-5">
-                <form>
+            <Formik {...formikProps}>
+                { formik => (
+                    <Form>
                     <label htmlFor="firstname">First name</label>
-                    <input 
-                        className="form-control" 
-                        type="text" 
-                        name="firstname"
-                    />
+                    <Field name="firstname" type="text" className="form-control"/>
+                    {/* <ErrorMessage name="firstname"/> */}
+                    { formik.errors.firstname && formik.touched.firstname ?
+                         <span>{formik.errors.firstname}</span>
+                    :null}
                     
                     <hr className="mb-4" />
+
+                    <label htmlFor="color">Color</label>
+                    <Field 
+                        as="select"
+                        name="color" 
+                        className="custom-control"
+                    >
+                        <option value="red">Red</option>
+                        <option value="green">Green</option>
+                        <option value="blue">Blue</option>
+                    </Field>
+
                     <button className="btn btn-primary btn-lg btn-block" type="submit">
                         Submit
                     </button>
-                </form>
+                    </Form>
+                )}
+            </Formik>
             </div>
         </div>
         
